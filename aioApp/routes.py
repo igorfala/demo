f1e276babf4b5450839ce74e51ce7ead6b84ad76
Igor Fala <ifala88@gmail.com>
2017-03-03 16:12:52 -0500

    fix STATIC_DIR path

--- aioApp/routes.py
+++ aioApp/routes.py
@@ -1,8 +1,9 @@
 from aioApp.views import index
-from aioApp.settings import BASE_DIR
+from aioApp.settings import BASE_DIR, STATIC_DIR
+import os
 
 def setup_routes(app):
     app.router.add_get('/', index)
     app.router.add_static('/static/',
-                      path=BASE_DIR + '/static',
+                      path=STATIC_DIR,
                       name='static')
