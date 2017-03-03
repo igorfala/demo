10c18dea903abaa3c78b0ebcc29da4d1bc9fe5d5
Igor Fala <ifala88@gmail.com>
2017-03-03 17:04:00 -0500

    changed DATABASE URL for heroku env

--- aioApp/settings.py
+++ aioApp/settings.py
@@ -1,5 +1,7 @@
 import os
 
+DEBUG = False  # Set False in Production, True in stagging
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CURR_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
