418472d3f65eb95851077be2930069d697b9f1c3
Igor Fala <ifala88@gmail.com>
2017-03-11 21:53:29 -0500

    key encode before creating hmac

--- shopify/helpers/proxy_check.py
+++ shopify/helpers/proxy_check.py
@@ -7,13 +7,16 @@ def get_proxy_signature(query_dict, secret):
     See: http://docs.shopify.com/api/tutorials/application-proxies#security
     """
     # Convert secret to byte array
-    secret = bytearray(secret, encoding='utf-8')
+    #secret = bytearray(secret, encoding='utf-8')
     # Sort and combine query parameters into a single string.
     sorted_params = ''
     for key in sorted(query_dict.keys()):
-        sorted_params += "{0}={1}".format(key, ",".join(list(query_dict[key])))
-
-    signature = hmac.new(secret, sorted_params, hashlib.sha256)
+        v = query_dict[key]
+        if isinstance(v, list):
+             v = ",".join(query_dict[key])
+        sorted_params += "{0}={1}".format(key, v)
+    print(sorted_params)
+    signature = hmac.new(secret.encode(), sorted_params.encode(), hashlib.sha256)
     return signature.hexdigest()
 
 
@@ -25,16 +28,18 @@ def proxy_signature_is_valid(query_dict, secret):
 
     # Extract the signature we're going to verify. If no signature's present, the request is invalid.
     try:
-        signature_to_verify = query_dict.pop('signature')[0]
+        signature_to_verify = query_dict.pop('signature')
         print(signature_to_verify)
     except KeyError:
         return False
 
     calculated_signature = get_proxy_signature(query_dict, secret)
-
+    print(calculated_signature)
     # Try to use compare_digest() to reduce vulnerability to timing attacks.
     # If it's not available, just fall back to regular string comparison.
     try:
+        print('try')
         return hmac.compare_digest(calculated_signature, signature_to_verify)
     except AttributeError:
+        print('AttributeErrorerror')
         return calculated_signature == signature_to_verify
